601de12778d980974c404a5fe1fd20ee38bd5f6b
mylesgwapa <20221232@nbsc.edu.ph>
2025-02-20 13:58:28 +0800

    Updates

--- assets/swipe-back-legacy-DCfJsSyh.js
+++ assets/swipe-back-legacy-DCfJsSyh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DoHvjtZ9.js","./index3-legacy-DxVGLs2S.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-UkHWFAIm.js","./index3-legacy-BtEkryNW.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */

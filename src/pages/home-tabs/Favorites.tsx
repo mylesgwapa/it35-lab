3b40177c619f6502c8335a845c4275b9ec919586
mylesgwapa <20221232@nbsc.edu.ph>
2025-02-20 13:44:39 +0800

    Tabs setup and /home-tabs/pages

--- src/pages/home-tabs/Favorites.tsx
+++ src/pages/home-tabs/Favorites.tsx
@@ -20,9 +20,19 @@ import {
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}
+        >
+          Favorites
+        </div>
         </IonContent>
       </IonPage>
     );
   };
   
-  export default Favorites;
+  export default Favorites; 

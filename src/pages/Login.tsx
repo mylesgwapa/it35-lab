841dace28103f9594cde239edfd7cde21d73e296
jasiel333 <kaisermeatriderr@gmail.com>
2025-02-27 14:26:37 +0800

    added components in login.tsx and feed.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,40 +1,95 @@
 import { 
-    IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar,
-      useIonRouter
-  } from '@ionic/react';
-  
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
+  IonAvatar,
+  IonButton,
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonIcon, 
+  IonInput, 
+  IonInputPasswordToggle, 
+  IonItem, 
+  IonLabel, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar,
+  useIonRouter
+} from '@ionic/react';
+import { logoIonic } from 'ionicons/icons';
 
-    const doLogin = () => {
-        navigation.push('/it35-lab/app','forward','replace');
-    }
+const Login: React.FC = () => {
+  const navigation = useIonRouter();
 
-
-   return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        <IonButton onClick={() => doLogin()} expand="full">
-                Login
-            </IonButton>
-        </IonContent>
-      </IonPage>
-    );
+  const doLogin = () => {
+    navigation.push('/it35-lab/app','forward','replace');
   };
-  
-  export default Login;
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+            padding: '20px'
+          }}
+        >
+        
+          <IonItem style={{ marginBottom: '20px', padding: '0' }}>
+            <IonAvatar 
+              aria-hidden="true" 
+              style={{ 
+                width: '120px', 
+                height: '120px', 
+                borderRadius: '50%',  
+                overflow: 'hidden',   
+                border: 'none'        
+              }}
+            >
+              <img
+                alt="User Avatar"
+                src="https://scontent.fmnl4-7.fna.fbcdn.net/v/t39.30808-6/475234841_614656854583956_3344141762035701787_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=AdXPrO5ECEIQ7kNvgF5wEyI&_nc_oc=Adh1koyAx8PkuK_ydP1waTHph3oUZmSVfdzraCBn1QRZXiHvrOPEnSvUc6BYTeLHnNU&_nc_zt=23&_nc_ht=scontent.fmnl4-7.fna&_nc_gid=AlHd5s2KYYvfX35Zn9DqA3F&oh=00_AYAu8vREYBDTQWIzecqMSjtfUZACkf04v0ttOZM17pLzRQ&oe=67C5BA19"
+                style={{ width: '100%', height: '100%' }} 
+              />
+            </IonAvatar>
+          </IonItem>
+
+        
+          <IonItem style={{ width: '100%', maxWidth: '400px', marginBottom: '20px' }}>
+            <IonInput label="Username" placeholder="Enter username" style={{ width: '100%' }} />
+          </IonItem>
+
+       
+          <IonItem style={{ width: '100%', maxWidth: '400px', marginBottom: '20px' }}>
+            <IonInput
+              type="password"
+              label="Password"
+              value="NeverGonnaGiveYouUp"
+              style={{ width: '100%' }}
+            >
+              <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+            </IonInput>
+          </IonItem>
+
+       
+          <IonButton onClick={() => doLogin()} expand="full" style={{ maxWidth: '400px' }}>
+            Login
+          </IonButton>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,38 +1,71 @@
 import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const Feed: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Feed</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}
-        >
-          Feed
-        </div>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Feed;
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonAvatar
+} from '@ionic/react';
+
+const Feed: React.FC = () => {
+ 
+  const newsArticles = [
+    { 
+      title: 'Breaking News: New Tech Release', 
+      content: 'The latest tech gadget is out now. Get yours today!',
+      image: 'https://webusupload.apowersoft.info/apowercom/wp-content/uploads/2018/07/ibooks-iphone.jpg' 
+    },
+    { 
+      title: 'Sports Update: Local Team Wins', 
+      content: 'Our local team has won the championship. Congratulations!',
+      image: 'https://www.wideanglesoftware.com/touchcopy/images/TC2/transfer-ibooks-to-your-computer.jpg' 
+    },
+    { 
+      title: 'Weather Forecast: Sunny Days Ahead', 
+      content: 'The weather will be sunny for the next week. Great for outdoor activities.',
+      image: 'https://cms-tc.pbskids.org/parents/_generic600Wide/look-at-the-weather.jpg' 
+    },
+    { 
+      title: 'Health Tips: Stay Active', 
+      content: 'Experts recommend 30 minutes of exercise daily for good health.',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNO8Da6FZDhAo87LlAFhuCofEx2NxwZ9rrcA&s' 
+    },
+  ];
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Feed</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonList>
+          {newsArticles.map((article, index) => (
+            <IonItem key={index}>
+          
+              <IonAvatar slot="start">
+                <img alt={article.title} src={article.image} />
+              </IonAvatar>
+              
+              <IonLabel>
+                <h2>{article.title}</h2>
+                <p>{article.content}</p>
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Feed;

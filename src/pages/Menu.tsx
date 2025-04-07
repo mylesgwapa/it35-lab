b9e261905626c7dfc31bc601246165f0b5be42f8
xraymundx <raymundmacapundag113@gmail.com>
2025-04-07 14:08:49 +0800

    added edit profile pages

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -16,16 +16,19 @@ import {
       IonToolbar 
 
     } from '@ionic/react'
-    import {homeOutline, logOutOutline, rocketOutline} from 'ionicons/icons';
+    
+    import {homeOutline, logOutOutline, rocketOutline, settingsOutline} from 'ionicons/icons';
   import { Redirect, Route } from 'react-router';
   import Home from './Home';
   import About from './About';
   import Details from './Details';
+  import EditProfilePage from './EditProfilePage';
   
   const Menu: React.FC = () => {
     const path = [
         {name:'Home', url: '/it35-lab/app/home', icon: homeOutline},
         {name:'About', url: '/it35-lab/app/about', icon: rocketOutline},
+        {name:'Profile', url: '/it35-lab/app/profile', icon: settingsOutline},
     ]
   
     return (
@@ -61,6 +64,7 @@ import {
           <Route exact path="/it35-lab/app/home" component={Home} />
           <Route exact path="/it35-lab/app/home/details" component={Details} />
           <Route exact path="/it35-lab/app/about" component={About} />
+          <Route exact path="/it35-lab/app/profile" component={EditProfilePage} />
               <Route exact path="/it35-lab/app">
               <Redirect to="/it35-lab/app/home"/>
               </Route>
